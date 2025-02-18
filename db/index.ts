cf537f234f1630abca593c3e6086de17f7bee6ef
Huse Kivrak <huse7@pm.me>
2025-02-18 18:51:03 -0500

    minor

--- db/index.ts
+++ db/index.ts
@@ -1,8 +1,14 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 
+const connectionString = process.env.DATABASE_URL;
+if (!connectionString) {
+	console.error('No POSTGRES_URL provided');
+	process.exit(1);
+}
+
 // Disable prefetch as it is not supported for "Transaction" pool mode
-const client = postgres(process.env.DATABASE_URL!, { prepare: false });
+const client = postgres(connectionString, { prepare: false });
 const db = drizzle({ client, logger: true });
 
 export default db;
