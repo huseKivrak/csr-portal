32bea84ac4ae097d27d4ecd2ef6ffb9a33919ea5
Huse Kivrak <huse7@pm.me>
2025-02-25 14:45:15 -0500

    debug: linting errors

--- app/(csr)/users/[id]/page.tsx
+++ app/(csr)/users/[id]/page.tsx
@@ -2,12 +2,13 @@ import { UserDetailView } from '@/components/views/UserDetailView';
 import { notFound } from 'next/navigation';
 import { generateDetailedUsersData } from '@/lib/db/actions/queries';
 
+type tParams = Promise<{ id: string; }>;
 export default async function UserPage({
   params,
 }: {
-  params: { id: string; };
+  params: tParams;
 }) {
-  const { id } = params;
+  const { id } = await params;
   const users = await generateDetailedUsersData();
   const userDetail = users.find((user) => user.user.id === parseInt(id));
 
