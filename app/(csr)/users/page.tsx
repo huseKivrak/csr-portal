558fd0aef42e7649b9000293db0c3efcd6777386
Huse Kivrak <huse7@pm.me>
2025-02-25 12:29:41 -0500

    minor

--- app/(csr)/users/page.tsx
+++ app/(csr)/users/page.tsx
@@ -1,26 +1,22 @@
-import { SelectUser } from "@/db/types";
-import { DataTable } from "@/components/ui/data-table/data-table";
-import { columns } from "./columns";
-import { generateUsersTableData } from "@/db/queries";
+
+import { generateDetailedUsersData } from "@/lib/db/actions/queries";
+import { columns } from './columns';
+import { DataTable } from '@/components/data-table/data-table';
 import { Suspense } from 'react';
-import { SkeletonTable } from "@/components/ui/data-table/skeleton-table";
-
-async function getData() {
-  const users = await generateUsersTableData();
-  return users;
-}
-
+import { DataTableSkeleton } from '@/components/data-table/data-table-skeleton';
 
 export default async function UsersPage() {
-  const data = await getData();
 
+  const data = await generateDetailedUsersData();
 
   return (
     <div className="container mx-auto py-10">
-      <Suspense fallback={<SkeletonTable />}>
+      <div className="flex justify-between items-center">
+        <h1 className="text-2xl font-bold">Users</h1>
+      </div>
+      <Suspense fallback={<DataTableSkeleton />}>
         <DataTable columns={columns} data={data} />
       </Suspense>
-
     </div>
   );
 }
