15ce6dee921bf71404ec714863c8d56c368d1fbc
Huse Kivrak <huse7@pm.me>
2025-02-25 12:19:26 -0500

    minor: disabled theme toggle for now

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -6,13 +6,13 @@ import { Breadcrumbs } from '@/components/ui/app-breadcrumbs';
 export function Navbar() {
   return (
     <nav className="bg-background">
-      <div className="flex h-16 items-center px-2 w-full justify-between">
+      <div className="flex h-12 items-center px-2 w-full justify-between">
         <div className="flex-1">
           <Breadcrumbs />
         </div>
-        <div className="flex items-center gap-4">
+        {/* <div className="flex items-center gap-4">
           <ModeToggle />
-        </div>
+        </div> */}
       </div>
     </nav>
   );
